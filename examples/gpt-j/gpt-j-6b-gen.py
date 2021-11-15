8bf49fed6f6e9d7ee2d2b4ad2da29361ef67ef93
Amin Rezaei <AminRezaei0x443@gmail.com>
2021-11-16 02:38:17 +0330

    feature: add examples of gpt-j

--- examples/gpt-j/gpt-j-6b-gen.py
+++ examples/gpt-j/gpt-j-6b-gen.py
@@ -1,15 +1,15 @@
 from transformers import AutoTokenizer
 from transformers.models.gptj.modeling_gptj import GPTJForCausalLM
 from transformers import AutoConfig
+from transformers import pipeline
 from pytorch_lit import LitModule
 
 if __name__ == "__main__":
     tokenizer = AutoTokenizer.from_pretrained("../../../lab/gpt-j-6B-f16")
-    config = AutoConfig.from_pretrained("../../../lab/gpt-j-6B-f16", output_attentions=True, output_hidden_states=True)
+    config = AutoConfig.from_pretrained("../../../lab/gpt-j-6B-f16")
     model = LitModule.from_params(".models/gpt-j-6b-lit",
                                   lambda: GPTJForCausalLM(config),
                                   device="cuda")
-    tokens = tokenizer("hello from pytorch-lit", return_tensors="pt")
-    tokens = {k: tokens[k].cuda() for k in tokens}
-    result = model(**tokens)
+    generator = pipeline('text-generation', model=model.module, tokenizer=tokenizer, device=0)
+    result = generator("Hello I am gpt-j model and ", max_length=14, num_return_sequences=1)
     print(result)
