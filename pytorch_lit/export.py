8d136e002f9e4e55ed467ad9e500b577f8301b96
Amin Rezaei <AminRezaei0x443@gmail.com>
2021-12-01 20:58:30 +0330

    feature: prepare with partial-loader + example

--- pytorch_lit/export.py
+++ pytorch_lit/export.py
@@ -5,6 +5,7 @@ from os import path
 import os
 
 from .util import write_json
+from .partial_loader import PartialLoader
 
 
 def prepare_params(parameters, save_dir, method="np-memmap", dtype='float32', progress=True):
@@ -13,6 +14,7 @@ def prepare_params(parameters, save_dir, method="np-memmap", dtype='float32', pr
     }
     if not path.exists(save_dir):
         os.makedirs(save_dir, exist_ok=True)
+    parameters = _standardize_params(parameters)
     if method == "np-memmap":
         _params_to_memmap(parameters, path.join(save_dir, "model.bin"),
                           path.join(save_dir, "model-meta.json"), dtype, progress)
@@ -25,19 +27,36 @@ def prepare_params(parameters, save_dir, method="np-memmap", dtype='float32', pr
     write_json(path.join(save_dir, "meta.json"), state)
 
 
+def _standardize_params(parameters):
+    if isinstance(parameters, dict):
+        return {
+            "keys": parameters.keys(),
+            "get": lambda d, k: d[k]
+        }
+    if isinstance(parameters, PartialLoader):
+        return {
+            "keys": parameters.keys(),
+            "get": lambda k: parameters.read_key(k)
+        }
+    raise RuntimeError(f"unsupported parameters type: {type(parameters)}")
+
+
 def _params_to_memmap(params, mem_file=None, meta_file=None, dtype='float32', progress=True):
     paramSize = lambda tensor: reduce(lambda x, y: x * y, tensor.shape, 1)
     startIndex = 0
     biggest = 0
     meta = {}
-    for i, k in enumerate(params):
-        size = paramSize(params[k])
+    # TODO: Merge Loops
+    get_param = lambda key: params["get"](key)
+    for i, k in enumerate(params["keys"]):
+        param = get_param(k)
+        size = paramSize(param)
         end = startIndex + size
         if size > biggest:
             biggest = size
         meta[k] = {
             "bound": (startIndex, end),
-            "shape": list(params[k].shape)
+            "shape": list(param.shape)
         }
         startIndex = end
     it = meta
@@ -47,7 +66,7 @@ def _params_to_memmap(params, mem_file=None, meta_file=None, dtype='float32', pr
     fp = np.memmap(mem_file, dtype=dtype, mode='write', shape=(startIndex,))
     for k in it:
         m = meta[k]
-        tV = params[k]
+        tV = get_param(k)
         b = m["bound"]
         fp[b[0]:b[1]] = tV.reshape(-1)
         fp.flush()
